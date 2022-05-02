ee7e755a8db5a3e6544ea40fb996fecd6d2ef5a5
kumar.charanswain <kumarswain367@gmail.com>
2022-05-02 20:29:57 +0530

    test file changes

---
+++ src/test/java/com/custom/postprocessing/PostProcessingApplicationTests.java
@@ -0,0 +1,12 @@
+/*
+ * package com.custom.postprocessing;
+ * 
+ * import org.junit.jupiter.api.Test; import
+ * org.springframework.boot.test.context.SpringBootTest;
+ * 
+ * @SpringBootTest class PostProcessingApplicationTests {
+ * 
+ * @Test void contextLoads() { }
+ * 
+ * }
+ */
